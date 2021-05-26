817c1b7af25514c6104eff2c5a936ca503219de5
guayana <isabelaguayanaz@gmail.com>
2021-05-26 19:11:59 -0300

    importando os componentes do material ui

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import { Card, CardContent } from './Card'
+import Button from './Button'
+import Grid from './Grid'
+import Skeleton from './Skeleton'
+import Select from './Select'
+import MenuItem from './MenuItem'
+import Typography from './Typography'
+
+export {
+  Button, 
+  Card,
+  CardContent,
+  Grid,
+  Skeleton,
+  MenuItem,
+  Typography,
+  Select
+}
